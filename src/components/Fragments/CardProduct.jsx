f3a7e4575f9b4984207dfa7e108217e173725401
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-06 22:28:27 +0700

    [ADD & UPDATE] Membuat rendering list yang mana pakai data array sehingga bisa bnayak data

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,57 +1,62 @@
-import React from 'react'
+// CardProduct.js
+import React from 'react';
 import ButtonFunction from '../Elements/Button/Index';
 
 const CardProduct = (props) => {
-  const {children} = props;
+  const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
-        {children}        
+    <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col">
+      {children}
     </div>
-  )
-}
+  );
+};
 
-
-const Body = (props) => {
-  const {children, title} = props;
-   return(
-    <div className="px-8 pb-5">
-    <a href="">
-    <h5 className='text-2xl font-semibold tracking-tight text-white'>{title}</h5>
-  
-    <p className="texy-m text-white">
-   {children}
-  </p>
-  </a>
-  </div>
-   )
-}
-
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * Body component of CardProduct
+ * 
+ * @param {Object} props - component props
+ * @param {String} props.title - title of the product
+ * @param {String} props.children - description of the product
+ * @returns {ReactElement} - Body component
+ */
+/******  ad545ac3-f97c-47bb-b76d-46a756723f6a  *******/const Body = (props) => {
+  const { children, title } = props;
+  return (
+    <div className="px-8 pb-5 flex-grow">
+      <a href="#">
+        <h5 className="text-2xl font-semibold tracking-tight text-white">{title}</h5>
+        <p className="text-sm text-white mt-2">{children}</p>
+      </a>
+    </div>
+  );
+};
 
 const Header = (props) => {
-  const {images} = props
-  return(
+  const { images } = props;
+  return (
     <a href="#">
-    <img src={images}
-         alt="product"
-          className='p-8 rounded-t-lg'/>
-  </a>
-  )
-}
+      <img
+        src={images}
+        alt="product"
+        className="w-full h-64 object-cover rounded-t-lg"
+      />
+    </a>
+  );
+};
 
 const Footer = (props) => {
-  const {prices} = props;
-  return(
-    <div className='flex items-center justify-between px-5 pb-5'>
-    <span className='text-xl font-bold text-white'>{prices}</span>
-    <ButtonFunction className='bg-blue-500'>add to cart</ButtonFunction>
-  </div>
-  )
-}
-
+  const { prices } = props;
+  return (
+    <div className="flex items-center justify-between px-5 pb-5">
+      <span className="text-xl font-bold text-white">{prices}</span>
+      <ButtonFunction className="bg-blue-500">Add to Cart</ButtonFunction>
+    </div>
+  );
+};
 
 CardProduct.Header = Header;
 CardProduct.Body = Body;
 CardProduct.Footer = Footer;
 
-
-export default CardProduct
+export default CardProduct;
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -4,14 +4,43 @@ import Button from '../components/Elements/Button/Index';
 import ButtonFunction from '../components/Elements/Button/Index';
 import CardProduct from '../components/Fragments/CardProduct';
 
+const products = [
+  {
+    id:1,
+    title:"Kopi",
+    images:"../images/kopi.jpg",
+    prices:"100000",
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis aperiam odio, similique ipsam quis, at, aliquid laudantium illum cum eveniet dolores perferendis tenetur quod velit saepe doloremque aspernatur officiis impedit",
+  },
+  {
+    id:2,
+    title:"Kopi",
+    images:"../images/kopi.jpg",
+    prices:"100000",
+    desc:"Lorem ipsum dolord velit saepe doloremque aspernatur officiis impedit",
+  },
+  {
+    id:3,
+    title:"Kopi",
+    images:"../images/kopi.jpg",
+    prices:"100000",
+    desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis aperiam odio, similique ipsam quis, at, aliquid laudantium illum cum eveniet ",
+  },
+
+]
+
 const ProductPages = () => {
   return (
-    <div className='flex justify-center py-5'>
-      <CardProduct>
-        <CardProduct.Header images="../images/kopi.jpg"></CardProduct.Header>
-      <CardProduct.Body title=" Kopi">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis aperiam odio, similique ipsam quis, at, aliquid laudantium illum cum eveniet dolores perferendis tenetur quod velit saepe doloremque aspernatur officiis impedit.</CardProduct.Body>
-        <CardProduct.Footern prices=""></CardProduct.Footern>
-      </CardProduct>
+    <div className='flex justify-center py-5 h-full'>
+      {products.map((product) => (
+            <CardProduct>
+            <CardProduct.Header images={product.images}></CardProduct.Header>
+          <CardProduct.Body title={product.title}>{product.desc}</CardProduct.Body>
+            <CardProduct.Footer prices={product.prices}></CardProduct.Footer>
+          </CardProduct>
+      ))}
+
+      
     </div>
   )
 }
