a33e8226bc3a60a9f42f171d7b1c2d2b14e06aa0
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-06 17:10:00 +0700

    [UPDATE} Implementasi nested component pada cardproducr dan product file

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,36 +1,57 @@
 import React from 'react'
 import ButtonFunction from '../Elements/Button/Index';
 
-const CardProduct = () => {
+const CardProduct = (props) => {
+  const {children} = props;
   return (
     <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow">
-        <a href="#">
-          <img src="/images/kopi.jpg" 
-               alt="product"
-                className='p-8 rounded-t-lg'/>
-        </a>  
-       <Body/>
-        <div className='flex items-center justify-between px-5 pb-5'>
-          <span className='text-xl font-bold text-white'>Rp. 1.000.000</span>
-          <ButtonFunction className='bg-blue-500'>add to cart</ButtonFunction>
-        </div>
-      </div>
+        {children}        
+    </div>
   )
 }
 
 
-const Body = () => {
+const Body = (props) => {
+  const {children, title} = props;
    return(
     <div className="px-8 pb-5">
     <a href="">
-      <h5 className='text-2xl font-semibold tracking-tight text-white'>Kopi</h5>
-    
+    <h5 className='text-2xl font-semibold tracking-tight text-white'>{title}</h5>
+  
     <p className="texy-m text-white">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ullam, minus dignissimos reiciendis ad ipsum, enim accusamus quaerat nam, provident commodi debitis recusandae cumque ipsam!
-    </p>
-    </a>
+   {children}
+  </p>
+  </a>
   </div>
    )
 }
+
+
+const Header = (props) => {
+  const {images} = props
+  return(
+    <a href="#">
+    <img src={images}
+         alt="product"
+          className='p-8 rounded-t-lg'/>
+  </a>
+  )
+}
+
+const Footer = (props) => {
+  const {prices} = props;
+  return(
+    <div className='flex items-center justify-between px-5 pb-5'>
+    <span className='text-xl font-bold text-white'>{prices}</span>
+    <ButtonFunction className='bg-blue-500'>add to cart</ButtonFunction>
+  </div>
+  )
+}
+
+
+CardProduct.Header = Header;
+CardProduct.Body = Body;
+CardProduct.Footer = Footer;
+
 
 export default CardProduct
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -7,7 +7,11 @@ import CardProduct from '../components/Fragments/CardProduct';
 const ProductPages = () => {
   return (
     <div className='flex justify-center py-5'>
-      <CardProduct/>
+      <CardProduct>
+        <CardProduct.Header images="../images/kopi.jpg"></CardProduct.Header>
+      <CardProduct.Body title=" Kopi">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis aperiam odio, similique ipsam quis, at, aliquid laudantium illum cum eveniet dolores perferendis tenetur quod velit saepe doloremque aspernatur officiis impedit.</CardProduct.Body>
+        <CardProduct.Footern prices=""></CardProduct.Footern>
+      </CardProduct>
     </div>
   )
 }
