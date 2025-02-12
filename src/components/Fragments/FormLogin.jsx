0d2bc07063264ef6ac7d74c0bed7e18b9c11f059
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-12 08:02:55 +0700

    UPDATE: Tes login dengan API pada form login

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -5,37 +5,45 @@ import { useRef ,useEffect} from 'react';
 import ButtonFunction from '../Elements/Button/Index';
 import InputForm from '../Elements/Input';
 
+import { login } from '../../services/auth-service';
+
 const FormLogin = () => {
   const handleLogin = (event) => {
     // AGAR preventDefault HALAMAN TIDAK TERREFRESH
     event.preventDefault();
 
     // simpan ke localhost
-    localStorage.setItem("email", event.target.email.value);
-    localStorage.setItem("password", event.target.password.value);
+   // localStorage.setItem("email", event.target.email.value);
+   // localStorage.setItem("password", event.target.password.value);
 
-    console.log(event.target.email.value);
-    console.log(event.target.password.value);
-    console.log("login");
+    //console.log(event.target.email.value);
+   // console.log(event.target.password.value);
+    //console.log("login");
 
-    window.location.href = "/products";
+   // window.location.href = "/products";\
+    const data = {
+      username: event.target.username.value,
+      password: event.target.password.value
+    }
+    login(data);
+
   }
 
-  const emailRef = useRef(null);
+  const usernameRef = useRef(null);
 
   useEffect(() => {
-  emailRef.current.focus();
+  usernameRef.current.focus();
     
   }, []);
 
   return (
     <form action="" onSubmit={handleLogin}>
-         <InputForm label="email" 
-                    type="email" 
-                    placeholder="email@gmail.com" 
-                    name="email"
-                    ref={emailRef} />
-          <InputForm label="form" type="password" placeholder="masukan password" name="password"></InputForm>
+         <InputForm label="username" 
+                    type="text" 
+                    placeholder="ahmada jarkakan" 
+                    name="username"
+                    ref={usernameRef} />
+          <InputForm label="password" type="password" placeholder="masukan password" name="password"></InputForm>
           <ButtonFunction 
               variant='bg-blue-700 w-full'
               type='submit'
